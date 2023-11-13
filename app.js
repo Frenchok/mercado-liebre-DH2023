95fb41ba4200e47922056dacc2f30c863b4f2800
Fede Coronel <fedecoronel@hotmail.com>
2023-11-13 18:46:55 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Configura la carpeta 'public' para archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Configura la carpeta 'views' para vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html'); // Asumiendo que tus archivos son HTML
+
+// Rutas
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`El servidor Express está escuchando en el puerto ${port}`);
+});
